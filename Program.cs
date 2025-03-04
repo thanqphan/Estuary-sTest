361e12d69641f61584ee54a647ac61705741b57a
Thang Phan <phanthq2k2@gmail.com>
2025-03-04 13:53:41 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+int lenghth = 14;
+string generateRandomString = GenerateRandomString(lenghth);
+Console.WriteLine("Generating random string: " + generateRandomString);
+
+string GenerateRandomString(int lenghth)
+{
+    string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    StringBuilder sb = new StringBuilder();
+    Random random = new Random();
+
+    for (int i = 0; i < lenghth; i++)
+    {
+        sb.Append(chars[random.Next(chars.Length)]);
+    }
+
+    return sb.ToString();
+}
