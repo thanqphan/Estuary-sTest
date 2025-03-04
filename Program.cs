862bc0f47421c68f7cb33a607c871f964d259e2b
Thang Phan <phanthq2k2@gmail.com>
2025-03-04 14:51:32 +0700

    - Smash Stone

--- Program.cs
+++ Program.cs
@@ -1,20 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text;
 
-int lenghth = 14;
-string generateRandomString = GenerateRandomString(lenghth);
-Console.WriteLine("Generating random string: " + generateRandomString);
+int[] arr1 = { 1, 2, 3 };
+int[] arr2 = { 2, 7, 4, 1, 8, 11 };
 
-string GenerateRandomString(int lenghth)
+Console.WriteLine("Last stone of arr1" + SmashStone(arr1));
+Console.WriteLine("Last stone of arr2" + SmashStone(arr2));
+
+int SmashStone(int[] arr)
 {
-    string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    StringBuilder sb = new StringBuilder();
-    Random random = new Random();
+    if (arr.Length == 0) return 0;
 
-    for (int i = 0; i < lenghth; i++)
+    var stones = arr.ToList();
+    while (stones.Count > 1)
     {
-        sb.Append(chars[random.Next(chars.Length)]);
+        stones.Sort((a, b) => b - a);
+        //max
+        int y = stones[0];
+        //2-max
+        int x = stones[1];
+        //remove 2 stones max n 2-max in array
+        stones.RemoveAt(0);
+        stones.RemoveAt(0);
+
+        if (x != y)
+        {
+            stones.Add(y - x);
+            //still have stones
+        }
     }
 
-    return sb.ToString();
+    return stones.Count == 0 ? 0 : stones[0];
 }
